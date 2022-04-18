08ff6c8654fdb3a9175d12b68132c373a4181820
Robin Dymér <robin.dymer@hotmail.com>
2022-04-19 00:28:01 +0200

    Fixed CD4 convergence

--- convergenceCD4.m
+++ convergenceCD4.m
@@ -9,7 +9,7 @@ T = 10;
 t = 0:k:T;
 y = zeros(1, length(t)); % Column vector
 y(1) = f; % Initial value
-y(2) = (1 + k^2/2*b) * f;
+y(2) = (1 + k^2/2*b + k^4/24*b^2) * f;
 
 for i=2:(length(t)-1)
     y(i+1) = -y(i-1) + 2*y(i) + k^2*(b+b^2*k^2/12)*y(i);
@@ -20,7 +20,7 @@ kk = 2*k;
 tt = 0:kk:T;
 yy = zeros(1, length(tt));
 yy(1) = f; % Initial value
-yy(2) = (1 + kk^2/2*b) * f;
+yy(2) = (1 + kk^2/2*b + kk^4/24*b^2) * f;
 
 for i=2:(length(tt)-1)
     yy(i+1) = -yy(i-1) + 2*yy(i) + kk^2*(b+b^2*kk^2/12)*yy(i);
